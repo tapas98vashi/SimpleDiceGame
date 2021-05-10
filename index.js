71f8203781f0d932c51f8bc81a6ab55f6027d626
tapas98vashi <44317106+tapas98vashi@users.noreply.github.com>
2021-05-10 15:40:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor((randomNumber1 * 6) + 1);
+var randomNumber2 = Math.random();
+randomNumber2 = Math.floor((randomNumber2 * 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins !";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins !";
+} else {
+  document.querySelector("h1").innerHTML = "Oops, There's a tie!";
+}
